817b47878cc8ff17e96ae8e68fc50387055d6268
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-26 22:29:38 +0900

    Add files via upload

---
+++ JavaScript_practiceII_loop_while statement.js
@@ -0,0 +1,11 @@
+// Please define a variable "number" and assign the number "1" to it.
+let number = 1 ;
+
+// Please create a while statement.
+// Please make sure that the variable "number" in the conditional expression is 100 or less.
+// Please print the value of the variable "number" in the while statement.
+// Also, on the line below, add 1 to the value of the variable "number".
+while (number <= 100) {
+  console.log(number) ;
+  number += 1 ;
+}
